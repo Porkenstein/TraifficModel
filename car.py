2afe1bf978f228409ef11803e6d54d488aba6802
Porkenstein <portenstein@gmail.com>
2016-05-08 22:37:49 -0600

    added nearly all functionality

--- car.py
+++ car.py
@@ -2,14 +2,18 @@
 from car import *
 import math
 from sys import *
-import numpy
+import numpy as np
 import random
 
 class Car:
+	_id = 0
+	_maxspeed = 300 #kmph
+	_maxpos = 30000 #m
 	#member functions 
 	def __init__(self, p_dict, tau, t):
 		'''Constructor initialized member variables.'''
-		self.id = id
+		self.id = Car._id
+		Car._id += 1
 		#the speed that the driver would like to go, V_n
 		#this is set by a normal distribution centered on 110 km/h
 		#with standard deviation of 10 km/h
@@ -30,37 +34,68 @@ class Car:
 		#the location of the front of this vehicle at time t-tsteo, x_n(t-tstep)
 		self.pos_prev = 0
 		#the speed of this car at time t, v_n(t)
-		self.speed = 0
+		self.speed = np.random.normal(110, 10)
 		#the speed of this car at time t - tstep, v_n(t-tstep)
-		self.speed_prev = 0
+		self.speed_prev = self.speed
 		# contains the probabilities for different events relevent to this single car
-		self.p_dict = p_dict
+		self.pmap = p_dict
 		# reaction time
 		self.tau = tau
 		# creation time
 		self.t0 = t
-
-	def update(self, tstep):
-		#setPosition
-		setPosition(tstep)
-		#setSpeed
-		setSpeed(tstep)
-		
+		# road raging?
+		self.angry = 0
+		# wrecked?
+		self.wrecked = 0
+	
+	# getters and setters for the next and previous cars
+	def getPrevCar(self):
+		if (self.prev_car is None):
+			prev_car = Car(None, 1, 1)
+			prev_car.size = 1
+			prev_car.pos = Car._maxpos
+			prev_car.speed = Car._maxspeed
+		else:
+			prev_car = self.prev_car
+		return prev_car
+	
+	def getNextCar(self):
+		if (self.next_car is None):
+			next_car = Car(None, 1, 1)
+			next_car.size = 1
+			next_car.pos = -1 * Car._maxpos
+			next_car.speed = 1
+		else:
+			next_car = self.next_car
+		return next_car
+	
 	def setSpeed(self, time):
 		'''A function that takes time and sets the speed value of the car.'''
 		#self.speed = v_n(t)
+		prev_car = self.getPrevCar()
 		self.speed_prev = self.speed
-		self.speed = min(self.speed + 2.5 * self.accel * Car.self.tau * (1 - (self.speed / self.vel)) * \
-			math.sqrt(0.025 + self.speed / self.vel), self.brake * Car.self.tau + math.sqrt(self.brake**2 \
-			* Car.self.tau**2 - self.brake * (2 * (self.prev_car.pos - self.prev_car.size - self.pos) - \
-			self.speed * Car.self.tau - (self.prev_car.speed**2) / min(-3.0, (self.brake - 3.0) / 2))))
-		
+		# TODO: make sure that cars aren't spawning on top of each other
+		try:
+			self.speed = min(self.speed + 2.5 * self.accel * self.tau * (1 - (self.speed / self.vel)) * \
+				math.sqrt(0.025 + self.speed / self.vel), self.brake * self.tau + math.sqrt(self.brake**2 \
+				* self.tau**2 - self.brake * (2 * (prev_car.pos - prev_car.size - self.pos) - \
+				self.speed * self.tau - (prev_car.speed**2) / min(-3.0, (self.brake - 3.0) / 2))))
+		except ValueError:
+			print("ValueError.  Taking Square root of " + str((self.brake**2 \
+				* self.tau**2 - self.brake * (2 * (prev_car.pos - prev_car.size - self.pos) - \
+				self.speed * self.tau - (prev_car.speed**2) / min(-3.0, (self.brake - 3.0) / 2)))))
+	
 	def __str__(self):
-		return "car " + str(self.id) + ": Speed " + str(self.speed) + ""
+		return "car " + str(self.id) + ": Speed " + str("%.2f" % self.speed) + "  Pos " + str("%.2f" % self.pos)
 
 	def setPosition(self, tstep):
 		'''A function that takes time and time step, and sets the position value of the car.
 		   Uses Butchers Method from the Gipps' Model Wikipedia page.'''
+		# "am I on top of the car in front of me?  If yes, go back a half meter at a time"
+		prev_car = self.getPrevCar()
+		while self.pos > (prev_car.pos - prev_car.size):
+			self.pos -= .5
+			
 		k1 = self.speed_prev
 		k3 = self.speed_prev + 0.25 * (self.speed - self.speed_prev)
 		k4 = self.speed_prev + 0.5 * (self.speed - self.speed_prev)
@@ -71,7 +106,20 @@ class Car:
 			       7 * k6) * tstep
 	def getPosition(self):
 		return self.pos
-	
+
+	def update(self, tstep):
+		#setPosition
+		self.setPosition(tstep)
+		#setSpeed
+		self.setSpeed(tstep)
+		
 	@staticmethod
 	def changeLane(car, lane):
 		pass
+		
+	@staticmethod
+	def remove(car, lane):
+		if not car.prev_car is None:
+			car.prev_car.next_car = car.next_car
+		if not car.next_car is None:
+			car.next_car.
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import numpy
 import random
 
 # constants
-DEBUG = True  #output debug info
-OUTPUT = True #output at each time step
+DEBUG = False #output debug info
+OUTPUT = False #output at each time step
 NLANES = 4
 TMIN = 0
 TMAX = 25200
@@ -17,6 +17,7 @@ IMPAIREDREACTIONTIME = 1.0
 SUNSET = 21600 # for the sake of example, it's at 6pm
 SUNSETLENGTH = 1800 # half an hour
 SUNSETMOD = .001 # sunset increases chance of accident every second by .1%
+WEATHERMOD = .001
 [SEDAN, SUV, COMPACT, MOTORCYCLE, BUS, SEMI] = range(0, 6)
 [ANGRY, NORMAL, BAD, GOOD, SLOW, UNOBSERVANT, FAST, EMERGENCY, DRUNK] = range(0,9)
 # 0 position is beginning of the lane
@@ -67,8 +68,9 @@ _car_sizes = [4.5, 5.3, 4.35, 2.1, 12.2, 17.5] #average lenths, in meters, of ca
 # when generating random event, use normal distribution with above properties.
 #    Compare to the P of the car's event from car.p_dict
 
+_weather = 0
 _sunset = 0
-_accidnet_mod = 0 # increases or decreases globally, making all drivers worse at avoiding accidents.  increases during sunset and poor weather.
+_accident_mod = 0 # increases or decreases globally, making all drivers worse at avoiding accidents.  increases during sunset and poor weather.
 
 def getNormal(key, mu_dict, sd_dict):
 	if sd_dict[key] is 0:
@@ -90,7 +92,7 @@ def buildDictionaries(roadfilename): # parse file.  Crazy code warning
 	
 	global _mu_dict, _onramp_loc, _offramp_loc, _archetypes_mu, _archetypes_sd, _car_sizes, _car_types, _timetable 
 	_timetable = lines[1:7]  # create chance to make new car at each hour, then assign the first one to the mu
-	[_mu_dict["BADWEATHER"], _mu_dict["MULTICARACCIDENT"], _mu_dict["TAKEEXIT"], _mu_dict["NEWCAR"]] = lines[0] + [_timetable[0][1]]
+	[_mu_dict["WEATHER"], _mu_dict["MULTICARACCIDENT"], _mu_dict["TAKEEXIT"], _mu_dict["NEWCAR"]] = lines[0] + [_timetable[0][1]]
 	_onramp_loc = lines[8]
 	_offramp_loc = lines[9]
 	for i in range(0, 9):
@@ -101,15 +103,43 @@ def buildDictionaries(roadfilename): # parse file.  Crazy code warning
 	return 1
 	
 def buildArchetypes(archetpyefilename):
+	# parse the input files
+	fin = open(archetpyefilename, mode="r")
+	if fin is None:
+		return 0	
+	lines = fin.readlines()
+	
+	for i in range(0, len(lines)):
+		lines[i] = lines[i].split()
+		for j in range(0, len(lines[i])):
+			lines[i][j] = float(lines[i][j])
+	print(lines)
+	
+	global _archetypes_mu, _archetypes_sd 
+	i = 0
+	while i < 18:
+		_archetypes_mu[i//2]["SPEEDING"] = lines[i][0]
+		_archetypes_mu[i//2]["ROADRAGE"] = lines[i][1]
+		_archetypes_mu[i//2]["SINGLECARACCIDENT"] = lines[i][2]
+		_archetypes_mu[i//2]["ENGINEFAILURE"] = lines[i][3]
+		_archetypes_mu[i//2]["CHANGELANE"] = lines[i][4]
+		_archetypes_sd[i//2]["SPEEDING"] = lines[i+1][0]
+		_archetypes_sd[i//2]["ROADRAGE"] = lines[i+1][1]
+		_archetypes_sd[i//2]["SINGLECARACCIDENT"] = lines[i+1][2]
+		_archetypes_sd[i//2]["ENGINEFAILURE"] = lines[i+1][3]
+		_archetypes_sd[i//2]["CHANGELANE"] = lines[i+1][4]
+		i = i + 2
 	return 1
 	
 def checkSunset(t):
 	if ( abs(t-SUNSET) < SUNSETLENGTH ):
 		if (not _sunset):
 			_accident_mod += SUNSETMOD
+			_sunset = 1
 			return 1
-	if (_sunset):
-		_accident_mod -= SUNSETMOD # consider making this continuous
+		if (_sunset):
+			_accident_mod -= SUNSETMOD # consider making this continuous
+			_sunset = 0
 	return 0
 	
 # RANDOM EVENTS
@@ -117,57 +147,120 @@ def checkSunset(t):
 #	cars do the side effect, and then return if they did it or not.
 
 def checkSingleCarAccident(car, lane):
+	if ( random.random() < car.pmap["SINGLECARACCIDENT"] ):
+		car.wrecked = 1
+		car.vel = 0
+		car.speed = 0
+		return 1
 	return 0
 	
-def checkMultiCarAccident(car1, car2, lane):
+def checkMultiCarAccident(car1, car2, lane): # far more probably than single car
+	if ( random.random() < _mu_dict["MULTICARACCIDENT"] ):
+		car.wrecked = 1
+		car.vel = 0
+		car.speed = 0
 	return 0
 	
 def checkRoadRage(car, lane):
+	if ( random.random() < car.pmap["ROADRAGE"] ):
+		if (not car.angry):
+			car.pmap = createPmap(ANGRY)
+			car.angry = 1
+			return 1
+		else:
+			car.pmap = createPmap(NORMAL)
+			car.angry = 0
 	return 0
+	
 	
 def checkWeather():
+	if ( random.random() < _mu_dict["WEATHER"] ):
+		if (not _weather):
+			_accident_mod += WEATHERMOD
+			_weather = 1
+			return 1
+		if (_weather):
+			_accident_mod -= WEATHERMOD # consider making this continuous
+			_weather = 0
 	return 0
 	
-def checkEngineFailure(car):
+def checkEngineFailure(car): # just treat like a wreck for now
+	if ( random.random() < car.pmap["ENGINEFAILURE"] ):
+		car.wrecked = 1
+		car.vel = 0
+		car.speed = 0
+		return 1
 	return 0
 	
-#def createPmap(): #creates a random Pmap based on what's in _mu_dict and _sd_dict
-#	return dict()
+def createPmap(i): #creates a random Pmap based on what's in _mu_dict and _sd_dict
+	pmap = {}
+	for key in _archetypes_mu[i]: # i is the archetype index
+		pmap[key] = getNormal(key, _archetypes_mu[i], _archetypes_sd[i]) # normal distribution to model differing behaviors
+	return pmap
+	
+def checkUpdateGlobals(t):
+	checkWeather()
+	checkSunset(t)
+	# update incoming traffic density
+	for i in range(0, len(_timetable)):
+		if t is _timetable[i][0]:
+			_mu_dict["CREATE"] = _timetable[i][1]
+			return 1
+	return 0
 	
 def checkCreateNewCar(mindist, lane, t): #shared between beginning of each lane
 	# TODO ajust pcreate by time of day
 	pcreate = _mu_dict["NEWCAR"]
-	if (lane[0].getPosition() > (lane[0].size + mindist)) and ( random.random() < pcreate):
-		lanes[l].insert(0, createCar(t))
+	if (random.random() < pcreate):
+		lane.insert(0, createCar(t))
 		return 1
 	return 0
 	
 def checkChangeLane(lanes, car):
+	dice = random.random()
+	if ( random.random() < car.pmap["CHANGELANE"] ):
+		if j is (len(lanes)-1):
+			Car.changeLane(car.next_car, lanes[j-1]) # pass to right if crash is leftmost
+		elif j is 0 or dice < .5:
+			Car.changeLane(car.next_car, lanes[j+1]) # prefer to pass on the left
+		else:
+			Car.changeLane(car.next_car, lanes[j-1]) # pass to right if crash is leftmost
+		return 1
 	return 0
 
-def checkTakeExit(car):
+def checkTakeExit(car, c, lanes, l):
+	if ( random.random() < _mu_dict["TAKEEXIT"] and l is 0): #rightmost lane
+		del lanes[l][c]
 	return 0
 	
 def updateCar(car, c, lanes, l, tstep, t, road_len):
-	#TODO change car attributes
-	car.update(tstep)
-	# resolve random events
-	checkChangeLane(lanes, car)
-	checkEngineFailure(car)
-	checkRoadRage(car, lane)
-	checkSingleCarAccident(car, lane) #check for multicar accident in the main loop
-	# check to see if the car is out of bounds and needs to be removed
 	if car.getPosition() >= road_len:
 		del lanes[l][c]
 		return 0
+	#TODO change car attributes
+	# resolve random events
+	checkChangeLane(lanes, car)
+	checkEngineFailure(car)
+	checkRoadRage(car, lanes[l])
+	checkSingleCarAccident(car, lanes[l]) #check for multicar accident in the main loop
+	# check to see if the car is out of bounds and needs to be removed
+	if not car.wrecked:
+		car.update(tstep)
 	return 1
 	
 def createCar(t): # different times of day have different chances
-	pmap = {}
-	
+	# determine the car type
+	roulette = random.random()
+	rtotal = 0
+	type = 0
+	for i in range(0, len(_car_types)):
+		rtotal += _car_types[i][0]
+		if rtotal > roulette:
+			type = i
+			break
 	# now determine the archetype
 	roulette = random.random()
-	pscalar = 1# add all ps for the selected vechile type.  1/this is the scalar value
+	pscalar = 9/sum( _car_types[type][1:9])# add all ps for the selected vechile type.  9/this is the scalar value
 	rtotal = 0
 	tau = SOBERREACTIONTIME
 	for i in range(0, len(_archetypes_mu)):
@@ -175,10 +268,13 @@ def createCar(t): # different times of day have different chances
 		if(rtotal >= roulette):
 			if i is DRUNK:
 				tau = IMPAIREDREACTIONTIME
-			for key in _archetypes_mu[i]:
-				pmap[key] = getNormal(key, _archetypes_mu[i], _archetypes_sd[i]) # normal distribution to model differing behaviors
-			return Car(pmap, tau, t)
+			pmap = createPmap(i)
+			car = Car(pmap, tau, t)
+			car.size = _car_sizes[type]
+			if DEBUG: print("NEW CAR of type " + str(type) + " and driver archetype "+ str(i) +" : " + str(car))
+			return car
 	return None # shouldn't happen
+
 
 
 if __name__ == "__main__":
@@ -203,7 +299,7 @@ if __name__ == "__main__":
 		
 	else:
 		print("Usage: main.py <parameterfile>")
-	if not buildDictionaries(roadname):
+	if not (buildDictionaries(roadname) and buildArchetypes(ARCHETYPEFILE)):
 		print("Error building dictionaries!")
 	elif DEBUG:
 		print(_mu_dict)
@@ -227,28 +323,35 @@ if __name__ == "__main__":
 			if not pcar is None:
 				pcar.next_car = ccar
 			lanes[j].append(ccar)
-			if DEBUG: print("Making Car " + str(i))
+			if DEBUG: print("Making Car " + str(ccar.id))
 			
 	fout = open(foutname, mode="w")
 	
 	# iterate at each time step
 	for t in range(0, tmax+1):
 		# resolve random environmental events
-		checkWeather()
-		checkSunset()
+		checkUpdateGlobals(t)
 		
 		for j in range(0, NLANES):
 			# resolve creation of new cars
-			checkCreateNewCar(0, 0, lanes[j])
+			
+			checkCreateNewCar(0, lanes[j], t)
 			
 			fout.write("\n\nT = " + str(t) + "------------\n")
 			if OUTPUT or DEBUG: print("\n\nT = " + str(t) + "------------\n")
-			for c in range(0, len(lanes[j])):
+			c = 0
+			while c < len(lanes[j]):
 				car = lanes[j][c]
-				updateCar(car, c, lanes, j, t, tstep, road_len)
-				checkMultiCarAccident(car, car.prev_car, lanes[j]) #does the car behind this one hit this car?
+				if(car.wrecked):
+					if not checkMultiCarAccident(car, car.next_car, lanes[j]): #does the car behind this one hit this car?
+						if j is (len(lanes)-1):
+							Car.changeLane(car.next_car, lanes[j-1]) # pass to right if crash is leftmost
+						else:
+							Car.changeLane(car.next_car, lanes[j+1]) # prefer to pass on the left
+				else:
+					updateCar(car, c, lanes, j, t, tstep, road_len)
 				fout.write("|  Lane "+str(j) + ", " + str(car) + "\n")
-				if OUTPUT or DEBUG: print("|  Lane "+str(j) + ", " + str(car) + "\n")
-				
+				#if OUTPUT or DEBUG: print("|  Lane "+str(j) + ", " + str(car) + "\n")
+				c = c+1
 	# finalize
 	fout.close()
